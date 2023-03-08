36c0d11264248bded5b90a3db48826c659710282
Dawid Klimowicz <dawidkl777@gmail.com>
2023-03-08 21:13:53 +0100

    first commit - gramapp

---
+++ android/app/src/main/kotlin/com/example/gramapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gramapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:gramapp/pages/login_page.dart';
+import 'package:gramapp/pages/register_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GramApp',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      initialRoute: 'login',
+      routes: {
+        'register': (context) => RegisterPage(),
+        'login': (context) => LoginPage(),
+      },
+    );
+  }
+}
